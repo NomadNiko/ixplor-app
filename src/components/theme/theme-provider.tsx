3f1c486e4fe6e0ef65c6cdc5a8faa09661e80c64
NomadNiko <nomad.niko.inc@gmail.com>
2025-01-06 00:16:31 -1000

    chore: update project files

--- src/components/theme/theme-provider.tsx
+++ src/components/theme/theme-provider.tsx
@@ -10,6 +10,57 @@ function ThemeProvider(props: PropsWithChildren<{}>) {
   const theme = useMemo(
     () =>
       createTheme({
+        palette: {
+          // Your custom colors
+          primary: {
+            main: '#2b1346', // your $purple
+            dark: '#14111b', // your $purple-dark
+            light: '#bd4de9', // your $purple-light
+          },
+          secondary: {
+            main: '#2196f3', // your $blue
+            dark: '#090a6e', // your $blue-dark
+            light: 'rgb(105, 201, 233)', // your $blue-light
+          },
+          error: {
+            main: '#ff0000', // your $red
+            light: '#e5a6a6', // your $red-light
+          },
+          success: {
+            main: '#009688', // your $green
+            light: '#00ff40', // your $green-light
+          },
+        },
+        typography: {
+          fontFamily: '"Exo 2", sans-serif',
+        },
+        components: {
+          MuiCssBaseline: {
+            styleOverrides: {
+              body: {
+                backgroundColor: '#14111b', // your $purple-dark
+                scrollbarWidth: 'auto',
+                scrollbarColor: '#2b1346 #14111b',
+                '&::-webkit-scrollbar': {
+                  width: '12px',
+                },
+                '&::-webkit-scrollbar-track': {
+                  background: '#14111b',
+                },
+                '&::-webkit-scrollbar-thumb': {
+                  backgroundColor: '#2b1346',
+                  border: '3px solid #14111b',
+                  borderRadius: '20px',
+                  backgroundClip: 'padding-box',
+                  '&:hover': {
+                    backgroundColor: '#3b1960',
+                    border: '2px solid #14111b',
+                  },
+                },
+              },
+            },
+          },
+        },
         cssVariables: {
           colorSchemeSelector: "class",
         },
