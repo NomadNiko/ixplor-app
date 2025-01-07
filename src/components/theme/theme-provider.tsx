908d60c0354de3b3aeac671019bf556ffff305b1
NomadNiko <nomad.niko.inc@gmail.com>
2025-01-06 20:00:45 -1000

    chore: update project files

--- src/components/theme/theme-provider.tsx
+++ src/components/theme/theme-provider.tsx
@@ -1,9 +1,6 @@
 "use client";
 
-import {
-  createTheme,
-  ThemeProvider as MuiThemeProvider,
-} from "@mui/material/styles";
+import { createTheme, ThemeProvider as MuiThemeProvider } from "@mui/material/styles";
 import { useMemo, PropsWithChildren } from "react";
 
 function ThemeProvider(props: PropsWithChildren<{}>) {
@@ -12,27 +9,27 @@ function ThemeProvider(props: PropsWithChildren<{}>) {
       createTheme({
         palette: {
           primary: {
-            main: "rgb(43, 19, 70)",    // #2b1346
-            dark: "rgb(20, 17, 27)",     // #14111b
-            light: "rgb(189, 77, 233)",  // #bd4de9
+            main: "rgb(43, 19, 70)",
+            dark: "rgb(20, 17, 27)",
+            light: "rgb(189, 77, 233)",
           },
           secondary: {
-            main: "rgb(33, 150, 243)",   // #2196f3
-            dark: "rgb(9, 10, 110)",     // #090a6e
-            light: "rgb(105, 201, 233)", // already in RGB
+            main: "rgb(33, 150, 243)",
+            dark: "rgb(9, 10, 110)",
+            light: "rgb(105, 201, 233)",
           },
           error: {
-            main: "rgb(255, 0, 0)",      // #ff0000
-            light: "rgb(229, 166, 166)", // #e5a6a6
+            main: "rgb(255, 0, 0)",
+            light: "rgb(229, 166, 166)",
           },
           success: {
-            main: "rgb(0, 150, 136)",    // #009688
-            light: "rgb(0, 255, 64)",    // #00ff40
+            main: "rgb(0, 150, 136)",
+            light: "rgb(0, 255, 64)",
           },
           text: {
-            primary: "rgb(255, 255, 255)",     // Always white
-            secondary: "rgb(255, 255, 255)",   // Also white for secondary text
-            disabled: "rgb(165, 165, 165)",    // Slightly dimmed for disabled text
+            primary: "rgb(255, 255, 255)",
+            secondary: "rgb(255, 255, 255)",
+            disabled: "rgb(178, 178, 178)",
           },
         },
         typography: {
@@ -42,23 +39,23 @@ function ThemeProvider(props: PropsWithChildren<{}>) {
           MuiCssBaseline: {
             styleOverrides: {
               body: {
-                backgroundColor: "rgb(20, 17, 27)",    // #14111b
+                backgroundColor: "rgb(20, 17, 27)",
                 scrollbarWidth: "auto",
-                scrollbarColor: "rgb(43, 19, 70) rgb(20, 17, 27)", // #2b1346 #14111b
+                scrollbarColor: "rgb(43, 19, 70) rgb(20, 17, 27)",
                 "&::-webkit-scrollbar": {
                   width: "12px",
                 },
                 "&::-webkit-scrollbar-track": {
-                  background: "rgb(20, 17, 27)",      // #14111b
+                  background: "rgb(20, 17, 27)",
                 },
                 "&::-webkit-scrollbar-thumb": {
-                  backgroundColor: "rgb(43, 19, 70)", // #2b1346
-                  border: "3px solid rgb(20, 17, 27)", // #14111b
+                  backgroundColor: "rgb(43, 19, 70)",
+                  border: "3px solid rgb(20, 17, 27)",
                   borderRadius: "20px",
                   backgroundClip: "padding-box",
                   "&:hover": {
-                    backgroundColor: "rgb(59, 25, 96)", // #3b1960
-                    border: "2px solid rgb(20, 17, 27)", // #14111b
+                    backgroundColor: "rgb(59, 25, 96)",
+                    border: "2px solid rgb(20, 17, 27)",
                   },
                 },
               },
