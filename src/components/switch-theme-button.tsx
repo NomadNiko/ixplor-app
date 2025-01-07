cc95b668e1bb90bc14f9efde864f6ee8f8ea6941
NomadNiko <nomad.niko.inc@gmail.com>
2025-01-06 23:07:45 -1000

    task: Fixing Mistakes

--- src/components/switch-theme-button.tsx
+++ src/components/switch-theme-button.tsx
@@ -1,27 +1,20 @@
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
 import IconButton from "@mui/material/IconButton";
+import { useColorScheme } from "@mui/material/styles";
 
 const ThemeSwitchButton = () => {
-  const toggleTheme = () => {
-    const root = document.documentElement;
-    const currentTheme = root.dataset.theme;
-    const newTheme = currentTheme === "purple" ? "darker" : "purple";
-    root.dataset.theme = newTheme;
-  };
-
-  // Get current theme to determine which icon to show
-  const currentTheme = typeof document !== 'undefined' 
-    ? document.documentElement.dataset.theme 
-    : 'purple';
+  const { colorScheme, setMode } = useColorScheme();
 
   return (
     <IconButton
       disableRipple
-      onClick={toggleTheme}
+      onClick={() => {
+        setMode(colorScheme === "light" ? "dark" : "light");
+      }}
       color="inherit"
     >
-      {currentTheme === "darker" ? (
+      {colorScheme === "dark" ? (
         <Brightness7Icon sx={{ width: 35, height: 35 }} />
       ) : (
         <Brightness4Icon sx={{ width: 35, height: 35 }} />
