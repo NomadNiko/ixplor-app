07d1c2c4c0cd9e33fa83afefd61937b2c00fd8f1
NomadNiko <nomad.niko.inc@gmail.com>
2025-01-08 19:06:04 -1000

    build: playing with the footer

--- src/app/[language]/finance/page.tsx
+++ src/app/[language]/finance/page.tsx
@@ -1,10 +1,6 @@
 import type { Metadata } from "next";
 import { getServerTranslation } from "@/services/i18n";
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid2";
-import Typography from "@mui/material/Typography";
-import FooterLink from "@/components/footer-link";
-import { Trans } from "react-i18next/TransWithoutContext";
+import FinanceContent from "./page-content";
 
 type Props = {
   params: Promise<{ language: string }>;
@@ -19,33 +15,6 @@ export async function generateMetadata(props: Props): Promise<Metadata> {
   };
 }
 
-export default async function Finance(props: Props) {
-  const params = await props.params;
-  const { t } = await getServerTranslation(params.language, "finance");
-
-  return (
-    <Container maxWidth="md">
-      <Grid
-        container
-        spacing={3}
-        wrap="nowrap"
-        pt={3}
-        direction="column"
-        sx={{ height: "90vh", justifyContent: "space-between" }}
-      >
-        <Grid size="grow">
-          <Typography variant="h3" data-testid="finance-title" gutterBottom>
-            {t("title")}
-          </Typography>
-          <Typography>
-            <Trans
-              i18nKey={`description`}
-              t={t}
-            />
-          </Typography>
-        </Grid>
-      </Grid>
-      <FooterLink />
-    </Container>
-  );
+export default function Page() {
+  return <FinanceContent />;
 }
