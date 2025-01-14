b4bafe4a96dd6abc4e480302eba0b1c54b5a1e0b
NomadNiko <nomad.niko.inc@gmail.com>
2025-01-14 02:35:37 -1000

    build: fixing schme and api

--- src/services/api/services/vendors.ts
+++ src/services/api/services/vendors.ts
@@ -3,12 +3,12 @@ import useFetch from '../use-fetch';
 import { API_URL } from '../config';
 import wrapperFetchJsonResponse from '../wrapper-fetch-json-response';
 import { RequestConfigType } from './types/request-config';
-import { VendorApiResponse, VendorResponse } from '@/app/[language]/types/vendor';
+import { VendorResponse } from '@/app/[language]/types/vendor';
 
 export function useGetVendorsService() {
   const fetch = useFetch();
   
-  return useCallback(async (requestConfig?: RequestConfigType): Promise<VendorApiResponse> => {
+  return useCallback(async (requestConfig?: RequestConfigType) => {
     const requestUrl = new URL(`${API_URL}/vendors`);
     const response = await fetch(requestUrl, {
       method: 'GET',
