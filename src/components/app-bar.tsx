4ae2c893c36b0295e685825fbfe3d5f193189458
NomadNiko <nomad.niko.inc@gmail.com>
2025-01-13 21:23:40 -1000

    build: testing custom font

--- src/components/app-bar.tsx
+++ src/components/app-bar.tsx
@@ -59,6 +59,7 @@ function ResponsiveAppBar() {
               mr: 2,
               display: { xs: "none", md: "flex" },
               fontWeight: 700,
+              fontFamily: 'iXplor',
               letterSpacing: ".3rem",
               color: "inherit",
               textDecoration: "none",
