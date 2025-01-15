99d3297467c6cf98bcd5536b7868e5c56e9fc87e
NomadNiko <nomad.niko.inc@gmail.com>
2025-01-14 16:08:00 -1000

    build: switching dependancy to new onboarding form

--- src/app/[language]/onboard/page-content.tsx
+++ src/app/[language]/onboard/page-content.tsx
@@ -3,7 +3,7 @@ import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import { useTranslation } from "@/services/i18n/client";
-import VendorOnboardingForm from "@/components/onboarding/vendor-onboarding-form";
+import VendorOnboardingForm from "@/components/onboarding/VendorOnboardingForm";
 
 export default function VendorOnboardingPage() {
   const { t } = useTranslation("onboard");
