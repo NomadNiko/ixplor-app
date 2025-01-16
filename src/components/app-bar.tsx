3389f04c95cf5c6c77f3aba3bc378d79b324be1f
NomadNiko <nomad.niko.inc@gmail.com>
2025-01-15 17:06:23 -1000

    feat: adding products to the app bar

--- src/components/app-bar.tsx
+++ src/components/app-bar.tsx
@@ -118,6 +118,19 @@ function ResponsiveAppBar() {
               {!!user?.role &&
                 [RoleEnum.ADMIN].includes(Number(user?.role?.id)) && [
                   <MenuItem
+                    key="products"
+                    onClick={handleCloseNavMenu}
+                    component={Link}
+                    href="/products"
+                  >
+                    <Typography textAlign="center">
+                      {t("common:navigation.products")}
+                    </Typography>
+                  </MenuItem>,
+                ]}
+              {!!user?.role &&
+                [RoleEnum.ADMIN].includes(Number(user?.role?.id)) && [
+                  <MenuItem
                     key="approvals"
                     onClick={handleCloseNavMenu}
                     component={Link}
@@ -220,6 +233,20 @@ function ResponsiveAppBar() {
                     href="/onboard"
                   >
                     {t("common:navigation.onboard")}
+                  </Button>
+                </>
+              )}
+
+            {!!user?.role &&
+              [RoleEnum.ADMIN].includes(Number(user?.role?.id)) && (
+                <>
+                  <Button
+                    onClick={handleCloseNavMenu}
+                    sx={{ my: 2, color: "white", display: "block" }}
+                    component={Link}
+                    href="/products"
+                  >
+                    {t("common:navigation.products")}
                   </Button>
                 </>
               )}
