bd921722e8f9879eba131bd1833ff57efd8dec55
NomadNiko <nomad.niko.inc@gmail.com>
2025-01-16 15:44:34 -1000

    build: another fix

--- src/app/[language]/tests/edit-cards/page.tsx
+++ src/app/[language]/tests/edit-cards/page.tsx
@@ -3,10 +3,11 @@ import { getServerTranslation } from "@/services/i18n";
 import EditCardTestContainer from "./edit-card-test-container";
 
 type Props = {
-  params: { language: string };
+  params: Promise<{ language: string }>;
 };
 
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
+export async function generateMetadata(props: Props): Promise<Metadata> {
+  const params = await props.params;
   const { t } = await getServerTranslation(params.language, "tests");
   return {
     title: t("editCardTest"),
