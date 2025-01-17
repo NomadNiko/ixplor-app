63c0e287810ee22a0fff64e47d84871d6e4c0ad7
NomadNiko <nomad.niko.inc@gmail.com>
2025-01-16 15:28:17 -1000

    build: fixing typing

--- src/app/[language]/tests/edit-cards/page.tsx
+++ src/app/[language]/tests/edit-cards/page.tsx
@@ -2,17 +2,18 @@ import type { Metadata } from "next";
 import { getServerTranslation } from "@/services/i18n";
 import EditCardTestContainer from "./edit-card-test-container";
 
-type Props = {
+interface PageProps {
   params: { language: string };
-};
+  searchParams?: { [key: string]: string | string[] | undefined };
+}
 
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
   const { t } = await getServerTranslation(params.language, "tests");
   return {
     title: t("editCardTest"),
   };
 }
 
-export default function Page() {
+export default function EditCardsTestPage() {
   return <EditCardTestContainer />;
 }
