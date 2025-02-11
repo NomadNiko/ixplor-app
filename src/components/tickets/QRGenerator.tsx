bb6779d36c73e364c3151743acd0426ee88945a8
NomadNiko <nomad.niko.inc@gmail.com>
2025-02-10 19:05:56 -1000

    build: qrCode Changes

--- src/components/tickets/QRGenerator.tsx
+++ src/components/tickets/QRGenerator.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { QRCodeSVG } from 'qrcode.react';
+import React from "react";
+import { QRCodeSVG } from "qrcode.react";
 
 interface QRGeneratorProps {
   ticketId: string;
@@ -8,11 +8,11 @@ interface QRGeneratorProps {
 
 const QRGenerator = ({ ticketId, transactionId }: QRGeneratorProps) => {
   const validationUrl = `${window.location.origin}/validation?ticketId=${ticketId}&transactionId=${transactionId}`;
-  
+
   return (
     <div className="flex flex-col items-center justify-center w-full p-4 space-y-4">
-      <p className="text-xs text-gray-400 text-center w-full"> {/* Adjusted text size and width */}
-        Vendors Scan to Verify
+      <p className="text-8 text-gray-800 text-center w-full">
+        Vendors Scan Here
       </p>
       <div className="flex justify-center items-center w-40 h-40 bg-slate-800 rounded-lg p-2">
         <QRCodeSVG
@@ -22,10 +22,19 @@ const QRGenerator = ({ ticketId, transactionId }: QRGeneratorProps) => {
           bgColor="#1C283A"
           fgColor="#CDDAF7"
           marginSize={4}
+          imageSettings={{
+            src: "https://ixplor-profile-s3-bucket-02.s3.us-east-2.amazonaws.com/0932659af73111d7ba4e2.png",
+            height: 60,
+            width: 60,
+            excavate: true,
+            opacity: 1,
+          }}
         />
       </div>
-      <p className="text-xs text-gray-400 text-center w-full"> {/* Adjusted text size and width */}
-        Vendors Scan to Verify
+      <p className="text-8 text-gray-800 text-center w-full">
+        {" "}
+        {/* Adjusted text size and width */}
+        Vendors Scan Here
       </p>
     </div>
   );
