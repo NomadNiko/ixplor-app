a8d9803c828389bdc07a98b6aeb310cabaf9505e
NomadNiko <nomad.niko.inc@gmail.com>
2025-03-12 13:39:16 -1000

    feat: implementing babel-plugin-react-compiler to handle rerenders inherent to react.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   // eslint: {
   //   dirs: ["src", "playwright-tests"],
   // },
+
+  experimental: {
+    swcPlugins: [
+      ['babel-plugin-react-compiler', {}]
+    ]
+  }
 };
 
 module.exports = nextConfig;
